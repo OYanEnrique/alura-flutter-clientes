647ac51c66979b72de07c13a7b05433c11a25bb2
OYanEnrique <enrique4work@gmail.com>
2026-01-26 14:23:48 -0300

    feat: adicionar classe Clients com ChangeNotifier para gerenciamento de estado

---
+++ lib/models/clients.dart
@@ -0,0 +1,9 @@
+import 'package:client_control/models/client.dart';
+import 'package:flutter/material.dart';
+
+class Clients extends ChangeNotifier{
+  List<Client> clients;
+
+  Clients({required this.clients});
+
+}
